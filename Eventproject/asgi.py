7e6022df72c033734a08ff98c0c001069ad647f0
Oluwagbemi Oluwasakin <51876261+Gbemi06@users.noreply.github.com>
2021-07-21 21:51:36 +0300

    user sign up form
    creating a user registration form

--- Eventproject/asgi.py
+++ Eventproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for EventProject project.
+ASGI config for Eventproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Eventproject.settings')
 
 application = get_asgi_application()
--- Eventproject/wsgi.py
+++ Eventproject/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for EventProject project.
+WSGI config for Eventproject project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EventProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Eventproject.settings')
 
 application = get_wsgi_application()
